c8c9c8e48dcdba8eaf127ef7c6f2467ff0ddc5e4
neinwechter <neinwechter@gmail.com>
2025-08-13 21:10:27 -0400

    Add usage details and debug cleanup

--- src/main.c
+++ src/main.c
@@ -6,12 +6,21 @@
 //#include "file.h"
 //#include "parse.h"
 
+void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t-f <database file>\tThe employee database file (REQUIRED)\n");
+    printf("\t-n\t\t\tCreate new database file (defualt: NO)\n");
+    printf("\t-h\t\t\tDisplay usage information\n");
+    printf("\n");
+    return;
+}
+
 int main(int argc, char *argv[]) {
     char *filepath = NULL;
     bool newfile =  false;
     int c;
 
-    while ((c = getopt(argc, argv, "nf:")) != -1) {
+    while ((c = getopt(argc, argv, "nhf:")) != -1) {
         switch (c) {
             case 'n':
                 newfile = true;
@@ -19,6 +28,9 @@ int main(int argc, char *argv[]) {
             case 'f':
                 filepath = optarg;
                 break;
+            case 'h':
+                print_usage(argv);
+                return 0;
             case '?':
                 printf("Unknown option -%c\n", c);
                 break;
@@ -27,8 +39,11 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    printf("Newfile: %d\n", newfile);
-    printf("File Path: %s\n", filepath);
+    if (filepath == NULL) {
+        printf("ERROR: Filepath (-f <filename>) is a required argument\n\n");
+        print_usage(argv);
+        return 0;
+    }
 
     return 0;
 }
