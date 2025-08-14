53de1ef643ffc58c483ff54a8d66fb5e3dd27f19
neinwechter <neinwechter@gmail.com>
2025-08-13 21:00:32 -0400

    Initial project structure and command line parsing

---
+++ src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <getopt.h>
+#include <stdbool.h>
+
+//#include "common.h"
+//#include "file.h"
+//#include "parse.h"
+
+int main(int argc, char *argv[]) {
+    char *filepath = NULL;
+    bool newfile =  false;
+    int c;
+
+    while ((c = getopt(argc, argv, "nf:")) != -1) {
+        switch (c) {
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case '?':
+                printf("Unknown option -%c\n", c);
+                break;
+            default:
+                return -1; 
+        }
+    }
+
+    printf("Newfile: %d\n", newfile);
+    printf("File Path: %s\n", filepath);
+
+    return 0;
+}
